033328fc58449a341b8d654e02262fd998c60ab2
Rafael <rafaamattos@hotmail.com>
2020-01-15 13:58:03 -0300

    Building Services and Repository

---
+++ src/main/java/br/com/domain/acaipersonalizados/Service/ClientService.java
@@ -0,0 +1,15 @@
+package br.com.domain.acaipersonalizados.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.domain.acaipersonalizados.repository.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository clienteRepository;
+	
+	
+}
---
+++ src/main/java/br/com/domain/acaipersonalizados/Service/OrderService.java
@@ -0,0 +1,8 @@
+package br.com.domain.acaipersonalizados.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+}
